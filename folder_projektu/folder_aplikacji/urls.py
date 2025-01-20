3da6fe45b4688d28e6ac6e2d0fe4c8bba8dfb9d6
WeronikaKwiecien <wkwiecien3@st.swps.edu.pl>
2025-01-20 11:40:37 +0100

    lab09v2

--- folder_projektu/folder_aplikacji/urls.py
+++ folder_projektu/folder_aplikacji/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [    
     path('persons/', views.person_list),
     path('persons/<int:pk>/', views.person_detail),
+    path('persons/update/<int:pk>/', views.person_update_delete),
+    path('persons/delete/<int:pk>/', views.person_update_delete),
     path('osoby/', views.OsobaList.as_view(), name = 'osoba_list'),
     path('osoby/<int:pk>/', views.OsobaDetail.as_view(), name = 'osoba_detail'),
     path('osoby/search/<str:substring>/', views.osoba_search),
@@ -12,4 +14,5 @@ urlpatterns = [
     path('welcome/', views.welcome_view),
     path('persons_html/', views.person_list_html),
     path('persons_html/<int:id>', views.person_detail_html),
+    
 ]
